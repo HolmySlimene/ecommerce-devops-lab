6da882a0bbcefb055f8d5cbebce52d9d0664f826
HolmySlimene <helmyslimene@gmail.com>
2026-05-18 14:32:08 +0100

    fix public ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,12 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "web" {
   count         = 2
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
   key_name      = "vockey"
 
+  associate_public_ip_address = true
+
   tags = {
     Name = "devops-instance"
   }
-}
-
-output "instance_public_ips" {
-  value = aws_instance.web[*].public_ip
 }
